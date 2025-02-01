23544becfd5131310dd2d9032814b3b17087a2c4
DootDoot4567 <dootdoot4567@gmail.com>
2025-02-01 12:11:25 -0500

    created Nested Modal componets and created a store for reactivity

---
+++ my-app/src/routes/Nested/FormInput.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { showModal } from './store.js';
+    import Modal from './Modal.svelte';
+  </script>
+  
+  <button on:click={() => $showModal = true}>
+    Open Modal
+  </button>
+  
+  <Modal />
+  
---
+++ my-app/src/routes/Nested/Modal.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { showModal } from './store.js';
+    let dialog;
+  
+    // Reactive statement to show the modal
+    $: if (dialog && $showModal) {
+      dialog.showModal && dialog.showModal(); // Safeguard check to call showModal only if the function exists
+    }
+  
+    const closeModal = () => {
+      showModal.set(false); // Close the modal by setting the store to false
+    };
+  </script>
+  
+  <dialog bind:this={dialog} on:close={closeModal}>
+    <div>
+      <slot name="header" />
+      <hr />
+      <slot />
+      <hr />
+      <button on:click={closeModal}>Close</button>
+    </div>
+  </dialog>
+  
