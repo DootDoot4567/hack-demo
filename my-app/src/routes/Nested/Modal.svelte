dbb72543193baf6db89320cd2b9288ae229b07a6
DootDoot4567 <dootdoot4567@gmail.com>
2025-02-01 13:19:32 -0500

    fixed stylistic issues with modal pop ups

--- my-app/src/routes/Nested/Modal.svelte
+++ my-app/src/routes/Nested/Modal.svelte
@@ -13,22 +13,26 @@
     };
 </script>
 
-<dialog bind:this={dialog} on:close={closeModal}>
-    <div>
-        <slot name="header" />
-        <hr />
-        <slot />
-        <hr />
-        
-        <!-- Close Button with SVG -->
-        <button 
-            class="flex ms-auto inline-flex justify-center items-center absolute right-3 top-2" 
-            autofocus 
-            on:click={closeModal}>
+<dialog 
+    class="bg-transparent backdrop:bg-black backdrop:bg-opacity-50 p-0 m-0 border-none rounded-lg shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" 
+    bind:this={dialog} 
+    on:close={closeModal}>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            </svg>			  
-        </button>
-    </div>
+        <div>
+            <slot name="header" />
+            <hr />
+            <slot />
+            <hr />
+            
+            <!-- Close Button with SVG -->
+            <button 
+                class="flex ms-auto inline-flex justify-center items-center absolute right-3 top-2" 
+                autofocus 
+                on:click={closeModal}>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>			  
+            </button>
+        </div>
 </dialog>
