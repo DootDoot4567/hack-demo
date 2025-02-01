1efde71fb24bd6a4341c03fc85d733a295869ca1
DootDoot4567 <dootdoot4567@gmail.com>
2025-02-01 11:03:43 -0500

    formated the layout of pages.

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
+	import { page } from '@sveltejs/kit';
+	import Navbar from "./Nested/navigation.svelte";
 	import '../app.css';
 	let { children } = $props();
+	
+	
 </script>
 
-{@render children()}
+<!-- {@render children()} -->
+
+<div class="m-0 p-0">
+
+  	<Navbar />
+
+	<div>
+		<slot />
+	</div>
+  </div>
