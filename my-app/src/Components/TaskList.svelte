9005e8cb1406e4028301818dc6bd0c07575c76d3
DootDoot4567 <dootdoot4567@gmail.com>
2025-02-01 14:05:08 -0500

    going through commit history

--- my-app/src/Components/TaskList.svelte
+++ my-app/src/Components/TaskList.svelte
@@ -1,7 +1,10 @@
 <script>
     export let task = []
+    import TaskItem from './TaskItem.svelte'
     console.log(task)
 </script>
+
 {#each task as fb (fb.id)}
     <h3>{fb.text}</h3>
+    <TaskItem item={fb} on:delete-task />
 {/each}
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -14,24 +14,29 @@
 </main>
    -->
 
-<script>
+   <script>
     import TaskList from '../Components/TaskList.svelte'
     let background = 'Blue';
+    import TaskStats from '../Components/TaskStats.svelte'
     let task = [
     {
         id: 1,
-        text: 'Do the dishes',
-    },
-    {
-        id: 2,
-        text: 'Go to sleep',
-    },
-    {
-        id: 3,
+@@ -16,8 +15,17 @@
         text: 'Shower',
     }
 ]
+$: count = task.length
+$: finished = 0
+const deleteTask = (e) => {
+    const itemId = e.detail
+    task = task.filter((item => item.id != itemId))
+    finished++
+}
 </script>
+
 <main>
     <TaskList {task} />
+<main class="min-h-screen bg-red-700">
+    <TaskStats {count} {finished}/>
+    <TaskList {task} on:delete-task={deleteTask} />
 </main>
