31e8f2de1085a729ac00a350136139a1697f955d
DootDoot4567 <dootdoot4567@gmail.com>
2025-02-01 12:48:37 -0500

    Made funcitonal simple modal pop up

--- my-app/src/routes/Nested/FormInput.svelte
+++ my-app/src/routes/Nested/FormInput.svelte
@@ -1,11 +1,15 @@
 <script>
-    import { showModal } from './store.js';
+    import { showModal } from '../../store.js';
     import Modal from './Modal.svelte';
-  </script>
-  
-  <button on:click={() => $showModal = true}>
+</script>
+
+<button on:click={() => $showModal = true}>
     Open Modal
-  </button>
-  
-  <Modal />
-  
+</button>
+
+<Modal class="h-32 w-32 bg-red-500 dark:bg-indigo-500">
+    <h2 slot="header">Title</h2>
+    <div>
+        <p>Your custom content can go here.</p>
+    </div>
+</Modal>
--- my-app/src/routes/Nested/Modal.svelte
+++ my-app/src/routes/Nested/Modal.svelte
@@ -1,24 +1,34 @@
 <script>
-    import { showModal } from './store.js';
+    import { showModal } from '../../store.js';
     let dialog;
-  
+
     // Reactive statement to show the modal
     $: if (dialog && $showModal) {
-      dialog.showModal && dialog.showModal(); // Safeguard check to call showModal only if the function exists
+      dialog.showModal(); // Show the modal when $showModal is true
     }
-  
+
     const closeModal = () => {
       showModal.set(false); // Close the modal by setting the store to false
+      dialog.close(); // Close the <dialog> element itself
     };
-  </script>
-  
-  <dialog bind:this={dialog} on:close={closeModal}>
+</script>
+
+<dialog bind:this={dialog} on:close={closeModal}>
     <div>
-      <slot name="header" />
-      <hr />
-      <slot />
-      <hr />
-      <button on:click={closeModal}>Close</button>
+        <slot name="header" />
+        <hr />
+        <slot />
+        <hr />
+        
+        <!-- Close Button with SVG -->
+        <button 
+            class="flex ms-auto inline-flex justify-center items-center absolute right-3 top-2" 
+            autofocus 
+            on:click={closeModal}>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>			  
+        </button>
     </div>
-  </dialog>
-  
+</dialog>
